eb89ed5fa523d944188c1ea1e054f7a4cb343d86
JangWooJin <ehdwlsdnwls@naver.com>
2016-03-08 01:54:54 +0900

    make some class

--- CallofMyth/src/Monster.java
+++ CallofMyth/src/Monster.java
@@ -1,4 +1,14 @@
 
 public class Monster {
-
+	private int healthscore;
+	private int mindscore;
+	private String name;
+	private int speed;
+	
+	Monster(int healthscore,int mindscore,String name,int speed ){
+		this.healthscore = healthscore;
+		this.mindscore = mindscore;
+		this.name = name;
+		this.speed = speed;
+	}	
 }
